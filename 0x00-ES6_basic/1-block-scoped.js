f7b2e83ab16f2cf913a2c5fceb6a13a873c4fa76
Evarmedia <mishakmanuel@gmail.com>
2024-04-22 12:12:19 +0100

    fix: task1 correction

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Using let to declare variables with block scope
-  let task2 = true; // Using let to declare variables with block scope
+  const task = false; // Using let to declare variables with block scope
+  const task2 = true; // Using let to declare variables with block scope
 
   if (trueOrFalse) {
-    let task = true; // No need to redeclare with let, just assign the value
-    let task2 = false; // No need to redeclare with let, just assign the value
+    const task = true; // No need to redeclare with let, just assign the value
+    const task2 = false; // No need to redeclare with let, just assign the value
   }
 
   return [task, task2];
