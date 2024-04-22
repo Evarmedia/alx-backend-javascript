766d8974e9ffe45e968565a2e60d965d62ecc33d
Evarmedia <mishakmanuel@gmail.com>
2024-04-22 14:39:36 +0100

    fix: previous fix didnt work, trting this one

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,5 +1,3 @@
-const returnHowManyArguments = (...args) => {
+export default function returnHowManyArguments(...args) {
     return args.length;
-  };
-  
-  export default returnHowManyArguments;
+}
