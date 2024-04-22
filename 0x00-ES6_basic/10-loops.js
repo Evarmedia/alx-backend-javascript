2f252565dc7e01d716e12c8000d14b6f5a0e6905
Evarmedia <mishakmanuel@gmail.com>
2024-04-22 11:36:10 +0100

    chore: 'solutions for task10 completed'

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+    const newArray = [];
+    for (let value of array) {
+      newArray.push(appendString + value);
+    }
+    return newArray;
+  }
