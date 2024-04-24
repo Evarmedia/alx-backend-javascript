15af1f56c0ded3830afa25d6d3853d729d5e41ac
Evarmedia <mishakmanuel@gmail.com>
2024-04-24 09:19:08 +0100

    chore: "2. Catch me if you can!"

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({ status: 200, body: 'success' });
+    } else {
+      reject(new Error({}));
+      console.log('Got a response from the API');
+    }
+  });
+}
