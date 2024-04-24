39bd07fa8ebebe2361576b6ca6060a5a12ea5872
Evarmedia <mishakmanuel@gmail.com>
2024-04-24 10:27:12 +0100

    chore: "3. Handle multiple successful promises"

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
