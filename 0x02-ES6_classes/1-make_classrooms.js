9008f2f19babd176a8bc834f5ac82f3d502d9443
Evarmedia <mishakmanuel@gmail.com>
2024-04-25 16:15:43 +0100

    chore: Task#!

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+import Classroom from './0-classroom';
+
+function initializeRooms() {
+  return [
+    new Classroom(19),
+    new Classroom(20),
+    new Classroom(34),
+  ];
+}
+
+export default initializeRooms;
